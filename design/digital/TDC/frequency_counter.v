ee6d8084c700f0d6caedff2510e58fbe67b966fa
radityankn <raditya.ep.rep@gmail.com>
2026-08-03 00:55:58 +0700

    resume the TDC block completion, added synchronizations

--- design/digital/TDC/frequency_counter.v
+++ design/digital/TDC/frequency_counter.v
@@ -43,7 +43,7 @@ module frequency_counter #(parameter WIDTH = 8) (
     // range_count_register (0x0E, 0x0F, and 0x10) --> used to set what address we need the module to respond to.
     //      - 24 bit wide, divided into 8-bit high, mid, and low register. Contains ranging measurement result
     // pulse_count_register (0x11 and 0x12) --> used for WRITE process where we receive data
-    //      - 16 bit wide, divided into 8-bit high and low register. Contains doppler measurement result
+    //      - 24 bit wide, divided into 8-bit high, mid, and low register. Contains doppler measurement result
     // pulse_count_threshold_register (0x13 and 0x14) --> used for READ process where we send data
     //      - 16 bit wide, divided into high and low register. Contains doppler pulse count threshold. 
     // counter_control_status_register (0x15) --> used to poll or set the operation of the module
@@ -229,10 +229,10 @@ module frequency_counter #(parameter WIDTH = 8) (
                 ACK_O <= 1'b0;
                 RTY_O <= 1'b0;
             end
-            // if no activity is in progress, update the flag registers and the front facing register 
-            else begin
+        end
+        // if no activity is in progress, update the flag registers and the front facing register 
+        else begin
 
-            end
         end
     end
     
@@ -255,12 +255,8 @@ module frequency_counter #(parameter WIDTH = 8) (
     used to interact with the input signal and reference clock
     */
 
-    reg [15:0] pulse_count_internal;            //front-end measurement counter register, where counting happens and data stored before being pushed to the bus-facing register
+    reg [23:0] pulse_count_internal;            //front-end measurement counter register, where counting happens and data stored before being pushed to the bus-facing register
     reg [23:0] range_count_internal;            //front-end measurement counter register, where counting happens and data stored before being pushed to the bus-facing register
-    reg [1:0] measurement_state;       //state machine to indicate whether input signal rising edge is present or not
-    reg [1:0] measurement_next_state;
-    reg measurement_start_internal;                      //state of the measurement state machine from the last clock cycle
-    reg measurement_done_internal;
     reg counter_ready;
 
     localparam STATE_READY = 0;
@@ -268,16 +264,8 @@ module frequency_counter #(parameter WIDTH = 8) (
     localparam STATE_PULSE = 2;
     localparam STATE_FINISH = 3;
 
-    /*
-    reg measurement_begin_buffer_1;
-    reg measurement_begin_buffer_2;
-
     // buffer blocks here, it simply uses the incoming clock signal to propagates the input
-    always @(posedge signal_input) begin
-        measurement_begin_buffer_1 <= counter_control_reg[7];
-        measurement_begin_buffer_2 <= measurement_begin_buffer_1;
-    end
-    */
+    // TBD...
 
     // Rising-Falling Edge Detector Block
     reg input_previous_state;
@@ -288,128 +276,158 @@ module frequency_counter #(parameter WIDTH = 8) (
         input_previous_state <= signal_input; 
     end
 
-    // Counter Outer FSM begins here
+    // Trigger Output Signal Generator Block
+    reg [6:0] trigger_timer_internal;
+    reg trigger_signal_out;
     always @(posedge CLK_I) begin
         if (RST_I == 1'b1 or counter_control_status_register[RESET_COUNTER_BIT] == 1'b1) begin
-            measurement_state <= STATE_READY;
+            trigger_signal_out <= 1'b0;
+            trigger_timer_internal <= 1'b0; 
+        end
+        else if (measurement_start = 1'b1 and measurement_done_internal_flag == 1'b0) begin
+            if (trigger_timer_internal == 7'd100) begin
+                trigger_signal_out <= 1'b0;
+                trigger_timer_internal <= trigger_timer_internal;    
+            end
+            else begin
+                trigger_signal_out <= 1'b1;
+                trigger_timer_internal <= trigger_timer_internal + 1'b1;
+            end
+        end
+        else if (measurement_start = 1'b1 and measurement_done_internal_flag == 1'b1) begin
+            trigger_signal_out <= 1'b0;
+            trigger_timer_internal <= trigger_timer_internal;
         end
         else begin
-            measurement_state <= measurement_next_state;
+            trigger_signal_out <= 1'b0;
+            trigger_timer_internal <= 7'd0;
         end
     end
 
+
     // Ranging Mode FSM subroutine begins here
-    reg trigger_signal_out;
     reg range_timeout_internal_flag;
     reg range_finished_internal_flag;
     reg pulse_timeout_internal_flag;
-    reg [1:0] measurement_next_state_range;
+    reg measurement_pulse_start_internal_flag;
 
     always @(posedge CLK_I) begin
         if (RST_I == 1'b1 or counter_control_status_register[RESET_COUNTER_BIT] == 1'b1) begin
             range_count_internal <= 24'd0;
             counter_ready <= 1'b0;
-            measurement_start_internal <= 1'b0;
-            measurement_range_done_internal <= 1'b0;
             range_finished_internal_flag <= 1'b0;
             range_timeout_internal_flag <= 1'b0;
+            measurement_pulse_start_internal_flag <= 1'b0;
         end
-        else if (measurement_start_internal <= 1'b1) begin
-            if (counter_control_status_register[MEASUREMENT_RANGE_MODE_ENABLE] == 1'b1) begin
-                if (measurement_state == STATE_RANGE) begin
-                    if (range_count_internal < 24'd11) begin
-                        measurement_next_state_range <= STATE_RANGE;
-                        range_count_internal <= range_count_internal + 1'b1;
-                        trigger_signal_out <= 1'b1;
-                    end
-                    else if (range_count_internal >= 24'd714286) begin
-                        measurement_next_state_range <= STATE_IDLE;
-                        range_timeout_internal_flag <= 1'b1;
-                    end
-                    else if (rising_edge_detected == 1'b1) begin
-                        measurement_next_state_range <= STATE_PULSE;
+        else if (counter_control_status_register[MEASUREMENT_RANGE_MODE_ENABLE] == 1'b1) begin
+            if (measurement_start == 1'b1 && range_finished_internal_flag == 1'b0) begin
+                counter_ready <= 1'b0;
+                // When it's timeout, go to idle, do not continue because there won't be any pulse
+                if (range_count_internal >= 24'd714285) begin
+                    range_timeout_internal_flag <= 1'b1;
+                    range_finished_internal_flag <= 1'b1;
+                    range_count_internal <= 24'd0;
+                end
+                // When the ranging is finished (pulse detected)...
+                else if (rising_edge_detected == 1'b1) begin
+                    if (counter_control_status_register[MEASUREMENT_PULSE_MODE_ENABLE] == 1'b1) begin
                         range_finished_internal_flag <= 1'b1;
+                        range_count_internal <= range_count_internal;
+                        measuremet_pulse_start_internal_flag <= 1'b1;
                     end
                     else begin
-                        range_count_internal <= range_count_internal + 1'b1;
-                        trigger_signal_out <= 1'b0;
-                        measurement_next_state_range <= STATE_RANGE;
+                        range_finished_internal_flag <= 1'b1;
+                        range_count_internal <= range_count_internal;
+                        measurement_pulse_start_internal_flag <= 1'b0;
                     end
                 end
-                else if (measurement_state == STATE_READY) begin
-                    measurement_next_state_range <= STATE_RANGE;
-                end
                 else begin
-                    measurement_next_state_range <= STATE_READY;
+                    range_count_internal <= range_count_internal + 1'b1;
                 end
             end
-            else if (counter_control_status_register[MEASUREMENT_RANGE_MODE_ENABLE] == 1'b0) begin
-                if (measurement_state == STATE_READY) begin
-                    measurement_next_state_range <= STATE_PULSE
+            else if (measurement_start == 1'b1 && range_finished_internal_flag == 1'b1) begin
+                range_count_internal <= range_count_internal;
+                counter_ready <= 1'b0;
+                range_finished_internal_flag <= range_finished_internal_flag;
+                measurement_pulse_start_internal_flag <= measurement_pulse_start_internal_flag;
+                range_timeout_internal_flag <= range_timeout_internal_flag;
+            end 
+            else if (measurement_start == 1'b0 && range_finished_internal_flag == 1'b1) begin
+                range_count_internal <= 24'b0;
+                counter_ready <= 1'b1;
+                range_finished_internal_flag <= 1'b0;
+                range_timeout_internal_flag <= 1'b0;
+            end
+            else begin
+                range_count_internal <= range_count_internal;
+                counter_ready <= counter_ready;
+                measurement_pulse_start_internal_flag <= measurement_pulse_start_internal_flag;
+                range_finished_internal_flag <= range_finished_internal_flag;
+                range_timeout_internal_flag <= range_timeout_internal_flag;
+            end
+        end
+        else if (counter_control_status_register[MEASUREMENT_RANGE_MODE_ENABLE] == 1'b0) begin
+            if (counter_control_status_register[MEASUREMENT_PULSE_MODE_ENABLE] == 1'b1) begin
+                if (measurement_start == 1'b1 && range_finished_internal_flag == 1'b0) begin
+                    counter_ready <= 1'b0;
+                    range_finished_internal_flag <= 1'b1;
+                    measuremet_pulse_start_internal_flag <= 1'b1;
                 end
+                else if (measurement_start == 1'b0 && range_finished_internal_flag == 1'b1) begin
+                    counter_ready <= 1'b1;
+                    range_finished_internal_flag <= 1'b0;
+                    measuremet_pulse_start_internal_flag <= 1'b0;
+                end
+            end
+            else begin
+                // do nothing...
             end
         end
     end
 
     // Pulse Mode FSM subroutine begins here
+    reg measurement_pulse_done_internal_flag;
+
     always @(posedge signal_input) begin
-        case (measurement_state_machine)
-            16'd0 : begin
-                if (rst_i == 1 || counter_reset_internal == 1) begin 
-                    measurement_state_machine <= 16'd0;
-                    counter_ready <= 1'b0;
-                    measurement_began <= 1'b0;
-                    measurement_end <= 1'b0;
-                end else if (measurement_begin_buffer_2 == 1) begin
-                    measurement_began <= 1'b1;
-                    counter_ready <= 1'b0;
-                    measurement_state_machine <= measurement_state_machine + 1'b1;
-                end else begin
-                    counter_ready <= 1'b1;
-                    measurement_began <= 1'b0;
-                    measurement_end <= 1'b0;
-                    measurement_state_machine <= measurement_state_machine;
-                end
-            end
-            16'd10000 : begin
-                if (rst_i == 1 || ext_rst_i == 0 || counter_reset_internal == 1) begin 
-                    measurement_state_machine <= 16'd0;
-                end else if (measurement_begin_buffer_2 == 1'b0) begin
-                    measurement_state_machine <= 16'd0;
-                end else begin 
-                    measurement_state_machine <= measurement_state_machine;
-                    measurement_end <= 1'b1;
-                end
-            end
-            default : begin 
-                if (rst_i == 1 || ext_rst_i == 0 || counter_reset_internal == 1) begin 
-                    measurement_state_machine <= 16'd0;
-                end else measurement_state_machine <= measurement_state_machine + 1'b1;
-            end
-        endcase
-    end
-
-    //main clock measurement block here
-    always @(posedge reference_clk_main) begin
-        //push to bus-facing register if measurement is done
-        if (rst_i == 1'b1 || counter_reset_internal == 1'b1) measurement_count_internal <= 32'd0;
-        else if (measurement_began == 1'b1) begin 
-            status[1] <= 1'b1;
-		    if (measurement_end == 1'b1) begin
-				measurement_count_internal <= measurement_count_internal;
-            end else measurement_count_internal <= measurement_count_internal + 1'b1;
-        end else begin 
-            measurement_count_internal <= 32'd0;
-            status[1] <= 1'b0;
+        if (RST_I == 1 || counter_reset_internal == 1) begin 
+                measurement_state_machine <= 16'd0;
         end
+        else if (measurement_pulse_start_internal_flag == 1'b1 and measurement_pulse_done_internal_flag == 1'b0) begin
+            case (measurement_state_machine)
+                16'd0 : begin
+                    measurement_state_machine <= measurement_state_machine + 1'b1;
+                end
+                16'd9999 : begin                   
+                    measurement_state_machine <= 16'd0;
+                    measurement_pulse_done_internal_flag <= 1'b1;
+                end
+                default : begin 
+                    measurement_state_machine <= measurement_state_machine + 1'b1;
+                end
+            endcase
+        end
+        else if (measurement_pulse_start_internal_flag == 1'b0 and measurement_pulse_done_internal_flag == 1'b1) begin
+            measurement_pulse_done_internal_flag <= 1'b0;
+        end
+        else begin
+            // do nothing...
+        end 
     end
 
-    assign counter_reset_internal = counter_control_reg[0]; 
-    //assign register_window = phase_count_intermediate;
+    // Pulse clock duration measurement block here
+    always @(posedge CLK_I) begin
+        if (RST_I == 1'b1 || counter_control_status_register[RESET_COUNTER_BIT] == 1'b1) begin 
+            pulse_count_internal <= 24'd0;
+        end
+        else if (measurement_pulse_start_internal_flag == 1'b1) begin 
+		    if (measurement_pulse_done_internal_flag == 1'b1) begin
+				measurement_count_internal <= measurement_count_internal;
+            end 
+            else begin
+                measurement_count_internal <= measurement_count_internal + 1'b1;
+            end
+        end else begin 
+            measurement_count_internal <= 24'd0;
+        end
+    end 
 endmodule  
-
-
-//PR: 
-// - working on the analysis of the FSM, whether it is capable of entering the unintended state or not
-// - testing the behaviour of the module via simulation, for various condition
-// - deciding whether the counter module is better to be separated futher as submodules or not
